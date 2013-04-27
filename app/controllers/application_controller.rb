ddad7dc7f926e839ad93032dfcc0c2cfd6107eec
awsam <xyu.wang.0713@gmail.com>
2013-04-27 19:10:17 +0800

    add I18n for devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_filter :set_locale
   protect_from_forgery
+
+  def set_locale
+    I18n.locale = 'cn'
+    I18n.locale = params[:locale] if %w(cn en).include? params[:locale]
+  end
+
 end
