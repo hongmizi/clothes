f17e8b0ae8b8e24f6754cfe0e5e8fd7c320fe815
awsam <xyu.wang.0713@gmail.com>
2013-04-27 13:44:17 +0800

    initialize

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Clothes::Application.config.secret_token = 'ce77513e011899c75018cb1ea05e417989131ede29ce870ee6f1d7cc89c737dc3ad9cc875ab2603606c1013abdee01d9d2289b8cb5f3d17bab29c42c938a8e09'
