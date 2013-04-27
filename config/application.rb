e6059e64549fe588d512b2fcd56961cace99fe76
awsam <xyu.wang.0713@gmail.com>
2013-04-27 23:42:46 +0800

    fix the error in heroku

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ end
 
 module Clothes
   class Application < Rails::Application
+    config.assets.initialize_on_precompile = FALSE
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
