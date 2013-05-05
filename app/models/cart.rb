565543a93b6f5eb2e4f111d7d9aefda5cc253533
awsam <xyu.wang.0713@gmail.com>
2013-05-06 00:01:10 +0800

    add cart model

---
+++ app/models/cart.rb
@@ -0,0 +1,4 @@
+class Cart < ActiveRecord::Base
+  attr_accessible :user_id
+  validates :user_id, presence: true
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130505154958) do
+ActiveRecord::Schema.define(:version => 20130505160002) do
+
+  create_table "carts", :force => true do |t|
+    t.integer  "user_id"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "designs", :force => true do |t|
     t.string   "name"
