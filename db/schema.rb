41d89c35f42d0b6e3277908e7dfb3c27682e18be
awsam <xyu.wang.0713@gmail.com>
2013-05-07 16:18:45 +0800

    changes some views

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130505161832) do
+ActiveRecord::Schema.define(:version => 20130507042919) do
 
   create_table "carts", :force => true do |t|
     t.integer  "user_id"
@@ -71,6 +71,7 @@ ActiveRecord::Schema.define(:version => 20130505161832) do
     t.text     "description"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
+    t.string   "image_url"
   end
 
   create_table "users", :force => true do |t|
